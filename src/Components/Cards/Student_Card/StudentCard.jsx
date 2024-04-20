a93c040d59994a7064437b33a113d2230ef806af
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-20 16:05:20 +0200

    Added the all students components to the projects and made some modifications

--- src/Components/Cards/Student_Card/StudentCard.jsx
+++ src/Components/Cards/Student_Card/StudentCard.jsx
@@ -1,16 +1,18 @@
-const StudentCard = () => {
+const StudentCard = (props) => {
+	const { avatar, name, subject, location, city, contact } = props;
+
 	return (
 		<>
-			<div className='w-[330px] h-fit bg-white text-center shadow-lg rounded-md py-4 px-6'>
+			<div className='w-[320px] h-fit bg-white text-center shadow-lg rounded-md py-4 px-6'>
 				<div className='avatar'>
 					<div className='w-24 rounded-full ring ring-primary ring-offset-[#6673fc] ring-offset-2'>
-						<img src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+						<img src={avatar} alt={avatar} />
 					</div>
 				</div>
-				<h1 className='text-2xl font-bold p-2'>John Petrr</h1>
-				<p className='pb-1'>Science</p>
-				<p className='pb-1'>A-103, shyam gokul flats, Mahatma Road</p>
-				<p className='pb-4'>Mumbai</p>
+				<h1 className='text-2xl font-bold p-2'>{name}</h1>
+				<p className='pb-1'>{subject}</p>
+				<p className='pb-1'>{location}</p>
+				<p className='pb-4'>{city}</p>
 				<p className='pb-3'>(+91)-1234-567-89</p>
 				<button className='py-1 px-4 rounded-full bg-red-600 text-white'>
 					Read More
--- src/Pages/Students/All_Students/Grid_View/GridView.jsx
+++ src/Pages/Students/All_Students/Grid_View/GridView.jsx
@@ -1,9 +1,22 @@
 import StudentCard from "../../../../Components/Cards/Student_Card/StudentCard";
+import { allStudentsGridViewData } from "../../../../Constants/Constants";
 
 const GridView = () => {
 	return (
 		<>
-			<StudentCard />
+			<div className='grid lg:grid-cols-3 md:grid-cols-2 place-items-center gap-6'>
+				{allStudentsGridViewData.map((item) => (
+					<StudentCard
+						key={item.id}
+						avatar={item.avatar}
+						name={item.name}
+						subject={item.subject}
+						location={item.location}
+						city={item.city}
+						contact={item.contact}
+					/>
+				))}
+			</div>
 		</>
 	);
 };
