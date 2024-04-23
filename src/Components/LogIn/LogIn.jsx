3ab349aae299a50ad63634ebaa26a479433f3848
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-24 00:10:48 +0200

    Wroking on the login component

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 
+import image from "/src/assets/login.jpg";
+
 const Logout = () => {
 	return (
 		<div>
 			<div className='bg-2 flex items-center justify-center w-screen h-screen'>
 				<div className='w-[950px] h-fit bg-white rounded-md  flex justify-center items-center py-5 px-6 gap-6'>
-					<div className='bg-red-400 w-[420px] h-[500px]'></div>
-					<div className='bg-blue-500 w-[380px] h-[500px] rounded-lg overflow-hidden '>
+					<div className='bg-red-400 w-[420px] h-[500px]'>
 						<Link to='/'>Back to Home</Link>
+					</div>
+					<div className='bg-blue-500 w-[380px] h-[500px] rounded-lg overflow-hidden '>
+						<img
+							src={image}
+							alt={image}
+							className='object-cover w-full h-full'
+						/>
 					</div>
 				</div>
 			</div>
