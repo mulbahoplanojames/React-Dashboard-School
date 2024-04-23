93073075198c8ba42becdbd20966bb070a740852
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-23 23:57:52 +0200

    Wroking on the login component

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Logout = () => {
 	return (
 		<div>
-			<div className='h-screen w-screen flex justify-center items-center bg-2'>
-				<div className='w-[900px] h-fit bg-white rounded-md  flex justify-center items-center py-5 px-6'>
+			<div className='bg-2 flex items-center justify-center w-screen h-screen'>
+				<div className='w-[950px] h-fit bg-white rounded-md  flex justify-center items-center py-5 px-6 gap-6'>
 					<div className='bg-red-400 w-[420px] h-[500px]'></div>
-					<div className='bg-blue-500 w-[420px] h-[80%]'>
+					<div className='bg-blue-500 w-[380px] h-[500px] rounded-lg overflow-hidden '>
 						<Link to='/'>Back to Home</Link>
 					</div>
 				</div>
