b03a36a1c85c2cc14e0c6f41862a998b39eba117
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-05-08 13:23:41 +0200

    Working on the AddStudents component

--- src/Pages/Students/Add_Students/AddStudents_Form/AddStudentsForm.jsx
+++ src/Pages/Students/Add_Students/AddStudents_Form/AddStudentsForm.jsx
@@ -1,10 +1,34 @@
 const AddStudentsForm = () => {
   return (
     <>
-      <section className="h-52 w-full bg-white">
-        <div className=""></div>
+      <section className="h-52 w-full bg-white rounded-md shadow-md">
+        <h1 className=" px-5 py-2 text-lg font-semibold border-b-2 border-red-200 border-dotted">
+          Basic Information
+        </h1>
+        <div className=" px-5 py-10">
+          <Names />
+        </div>
       </section>
     </>
+  );
+};
+
+const Names = () => {
+  return (
+    <div className=" px-5 py-10">
+      <div className="gap-x-8 grid grid-cols-2">
+        <input
+          type="text"
+          placeholder="First Name"
+          className="inline-block w-full py-2 mr-5 border-b-2 outline-none"
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          className=" w-full py-2 border-b-2 outline-none"
+        />
+      </div>
+    </div>
   );
 };
 
