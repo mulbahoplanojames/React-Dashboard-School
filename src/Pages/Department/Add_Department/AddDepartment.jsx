a43905155334844014d8d260990eec6cd446efa4
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-05-10 23:46:54 +0200

    Started the Add Department component

--- src/Pages/Department/Add_Department/AddDepartment.jsx
+++ src/Pages/Department/Add_Department/AddDepartment.jsx
@@ -1,4 +1,5 @@
 import DashboardOneHeader from "../../../Components/Dashboard_1_Header/DashboardOneHeader";
+import AddDepartmentForm from "./AddDepartmentForm";
 
 /**
  * @description AddDepartment Component
@@ -9,8 +10,9 @@ const AddDepartment = () => {
 		// section container to hold everything on this page
 		<section className='md:px-5 bg-1 px-3 py-10'>
 			{/* Dashboard Header Component */}
-			<DashboardOneHeader text='Add Course' />
-			{/* AddCourseForm component to render the form */}
+			<DashboardOneHeader text='Add Department' />
+			{/* AddDepartmentForm component to render the form */}
+			<AddDepartmentForm />
 		</section>
 	);
 };
