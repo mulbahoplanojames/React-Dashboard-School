46e7cbd47b474d6c4ef754a6c0649dc765225b96
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-05-11 00:15:22 +0200

    Started the Edit Department component

--- src/Pages/Department/Edit_Department/EditDepartment.jsx
+++ src/Pages/Department/Edit_Department/EditDepartment.jsx
@@ -1,16 +1,18 @@
 import DashboardOneHeader from "../../../Components/Dashboard_1_Header/DashboardOneHeader";
+import EditDepartmentForm from "./EditDepartmentForm";
 
 /**
- * @description AddDepartment Component
- * This is a functional component that is responsible for rendering the AddDepartment Page
+ * @description EditDepartment Component
+ * This is a functional component that is responsible for rendering the EditDepartment Page
  */
 const EditDepartment = () => {
   return (
     // section container to hold everything on this page
     <section className="md:px-5 bg-1 px-3 py-10">
       {/* Dashboard Header Component */}
-      <DashboardOneHeader text="Add Department" />
-      {/* AddDepartmentForm component to render the form */}
+      <DashboardOneHeader text="Edit Department" />
+      {/* EditDepartmentForm component to render the form */}
+      <EditDepartmentForm />
     </section>
   );
 };
---
+++ src/Pages/Department/Edit_Department/EditDepartmentForm.jsx
@@ -0,0 +1,108 @@
+//Component for the Add Students Form page
+const EditDepartmentForm = () => {
+  const [departmentName, setDepartmentName] = useState("Computer Science");
+  const [headOfDepartment, setHeadOfDepartment] = useState("John Doe");
+  const [startDate, setStartDate] = useState("12-12-2022");
+  const [studentCapicity, setStudentCapicity] = useState("20");
+  const [details, setDetails] = useState(
+    "This department is about web development and mobile app development"
+  );
+
+  return (
+    <section className="h-fit w-full pb-10 bg-white rounded-md shadow-md">
+      {/* Header with the form title */}
+      <h1 className=" px-5 py-2 text-lg font-semibold border-b-[2px] border-gray-400 border-dotted">
+        Edit Department
+      </h1>
+      {/* Main form content */}
+      <div className=" px-5 py-6">
+        <DepartmentName_HeadOfDepartment
+          departmentName={departmentName}
+          setDepartmentName={setDepartmentName}
+          headOfDepartment={headOfDepartment}
+          setHeadOfDepartment={setHeadOfDepartment}
+        />
+        <DepartmentStartDate_StudentCapicity
+          startDate={startDate}
+          setStartDate={setStartDate}
+          studentCapicity={studentCapicity}
+          setStudentCapicity={setStudentCapicity}
+        />
+        <DepartmentDetails details={details} setDetails={setDetails} />
+      </div>
+      {/* Buttons to submit and cancel the form */}
+      <div className="pl-[24rem] ">
+        <button className="tw_btn mr-8 bg-blue-500">Submit</button>
+        <button className="tw_btn bg-red-500">Cancel</button>
+      </div>
+    </section>
+  );
+};
+
+const DepartmentName_HeadOfDepartment = (props) => {
+  const {
+    departmentName,
+    setDepartmentName,
+    headOfDepartment,
+    setHeadOfDepartment,
+  } = props;
+
+  return (
+    <div className=" px-5 py-10">
+      <div className="gap-x-8 grid grid-cols-2">
+        {/* First Name input field */}
+        <input
+          type="text"
+          placeholder="Department Name"
+          className="border-b-[2px] inline-block w-full py-2 mr-5 outline-none focus:border-blue-500"
+        />
+        {/* Last Name input field */}
+        <input
+          type="text"
+          placeholder="Head Of Department"
+          className=" w-full py-2 border-b-[2px] outline-none focus:border-blue-500 "
+        />
+      </div>
+    </div>
+  );
+};
+
+const DepartmentStartDate_StudentCapicity = () => {
+  return (
+    <div className=" px-5 pb-4">
+      <div className="gap-x-8 grid grid-cols-2">
+        <input
+          type="text"
+          required
+          name="startDate"
+          id="startDate"
+          onFocus={(e) => (e.target.type = "date")}
+          placeholder="Department Start Date"
+          className="inline-block w-full py-2 mr-5 border-b-[2px] outline-none focus:border-blue-500"
+        />
+        <input
+          type="text"
+          placeholder="Student Capicity"
+          className="w-full py-6 border-b-[2px] outline-none focus:border-blue-500"
+        />
+      </div>
+    </div>
+  );
+};
+
+const DepartmentDetails = () => {
+  return (
+    <div className=" px-5 py-10">
+      <input
+        type="text"
+        required
+        name="DepartmentDetails"
+        id="DepartmentDetails"
+        placeholder="Department Details"
+        className="inline-block w-full py-6 mr-5 border-b-[2px] outline-none focus:border-blue-500"
+      />
+    </div>
+  );
+};
+
+export default EditDepartmentForm;
